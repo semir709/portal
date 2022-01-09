e856150c01fd33e88b696819caa98428d8908d56
Semir Selman <selmansemir555@gmail.com>
2022-01-09 13:56:27 +0100

    adding the next page file

--- controllers/main.js
+++ controllers/main.js
@@ -3,5 +3,8 @@
 module.exports = {
     home: function(req, res) {
         res.render('home.ejs')
+    },
+    next: function(req, res) {
+        res.render('next_page.ejs')
     }
 }
--- routes/routes.js
+++ routes/routes.js
@@ -6,4 +6,7 @@ const main = require('../controllers/main');
 route.get('/', main.home);
 route.get('/home', main.home);
 
+route.get('/:page', main.next);
+route.get('/home:page', main.next);
+
 module.exports = route;
