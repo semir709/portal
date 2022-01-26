9aaa461ccd21e32ea745454af06e532c8090d289
Semir Selman <selmansemir555@gmail.com>
2022-01-26 13:19:10 +0100

    made admin all contents the page

--- public/js/admin.js
+++ public/js/admin.js
@@ -17,13 +17,32 @@ function myFunction() {
 
 }
 
-  $(".ss_list ").click(function() {
+$(".ss_list ").click(function() {
 
-      $('.ss_label').removeClass("clicked_list");
-      $('.ss_icon_nav').removeClass("clicked_list");
+    $('.ss_label').removeClass("clicked_list");
+    $('.ss_icon_nav').removeClass("clicked_list");
 
-      
-      this.querySelector('label').classList.toggle("clicked_list");
-      this.querySelector('i').classList.toggle("clicked_list");
-  });
+    
+    this.querySelector('label').classList.toggle("clicked_list");
+    this.querySelector('i').classList.toggle("clicked_list");
+});
+
+
+
+// all contents
+
+$(".ss_nav_a ").click(function() {
+
+    $(".ss_nav_a ").css("border-bottom", "none");
+
+    $(this).css("border-bottom", "2px solid #000000");
+    
+    
+});
+
+$("#label_search").click(function() {
+    let input_search = document.getElementById('search_input');
+
+    input_search.classList.toggle("ss_input_streach");
+});
 
