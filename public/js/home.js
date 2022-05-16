4a3811c29ac5669f255c1594448aa6cd01e57daa
Semir Selman <selmansemir555@gmail.com>
2022-05-16 16:24:27 +0200

    fixing the other list funcionality

--- public/js/home.js
+++ public/js/home.js
@@ -6,6 +6,32 @@ $('.ss_article').on('click', function() {
     //ajax
 });
 
+//navigation home
+$('#other_button').on('click', function() {
+
+    $('.ss_block_others').toggle('.ss_show_other');
+
+});
+
+//closing other content list on any diffrent click
+
+$(document).mouseup(function(e) 
+{
+    let container = $(".ss_block_others ");
+    let other_button_a = $('#other_button').children()[1];
+    let other_button_i = $('#other_button').children()[0];
+
+    if ((!container.is(e.target) && container.has(e.target).length === 0)
+        &&(!$(other_button_a).is(e.target) && $(other_button_a).has(e.target).length === 0)
+        &&(!$(other_button_i).is(e.target) && $(other_button_i).has(e.target).length === 0)) 
+    {
+
+        if(container.css('display') == 'block'){
+            container.toggle('ss_show_other');
+        } 
+    }
+});
+
 
 //click on the main content
 $('.ss_main_content_click').on('click', function() {
@@ -24,13 +50,6 @@ $('.content_holder').on('click', '.ss_content_holder', function() {
 
 });
 
-
-//navigation home
-$('#other_button').on('click', function() {
-
-    $('.ss_block_others').toggle('.ss_show_oher');
-
-});
 
 //click lists 
 $('.list_categroy_all').on('click', function() {
